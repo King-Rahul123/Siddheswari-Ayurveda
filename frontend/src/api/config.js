13d46b6170c221eec1828a8ac8bc5c4f622feab8
King-Rahul123 <adakrahul15@gmail.com>
2026-07-28 16:19:17 +0530

    deploy

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://inform-glass-retailers-mozilla.trycloudflare.com/api";
+export const API_BASE_URL = "https://gently-feature-nothing-hosting.trycloudflare.com/api";
