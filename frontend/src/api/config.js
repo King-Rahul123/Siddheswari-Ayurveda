7b17484c87b17d1d2fe451d5a3ecd604d8fa98fb
Rohit Kumar Adak <rohitadak0@gmail.com>
2026-08-09 08:31:34 +0530

    cloudflare update

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = "https://curve-computational-calling-movers.trycloudflare.com/api";
+export const API_BASE_URL = "https://exploration-difficulties-shoppers-passengers.trycloudflare.com/api";
