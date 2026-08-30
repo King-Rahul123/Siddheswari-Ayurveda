b48585a3aca6873857edc6cd7e09fcdfb214eaa3
King-Rahul123 <adakrahul15@gmail.com>
2026-08-30 16:39:38 +0530

    Update

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = "https://fields-calcium-les-scoring.trycloudflare.com/api";
+export const API_BASE_URL = "https://fields-calcium-les-scoring.trycloudflare.com/api";
 
-export const API_BASE_URL = "http://local/host:5000/api";
+// export const API_BASE_URL = "http://local/host:5000/api";
