f1ad520a513074c040a767a2f02d371d88adc95c
Rohit Kumar Adak <rohitadak0@gmail.com>
2026-08-30 14:55:22 +0530

    backend  server

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,3 +1,3 @@
-// export const API_BASE_URL = "https://fields-calcium-les-scoring.trycloudflare.com/api";
+export const API_BASE_URL = "https://fields-calcium-les-scoring.trycloudflare.com/api";
 
-export const API_BASE_URL = "http://localhost:5000/api";
+// export const API_BASE_URL = "http://localhost:5000/api";
