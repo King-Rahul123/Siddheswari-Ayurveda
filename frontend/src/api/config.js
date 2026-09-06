0f9728e5a1a135f05af9757207861850d86484ee
King-Rahul123 <adakrahul15@gmail.com>
2026-09-06 19:17:10 +0530

    Server Update

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,5 +1,5 @@
 export const API_BASE_URL =
-  import.meta.env?.VITE_API_BASE_URL || "https://placing-insured-perform-mating.trycloudflare.com/api";
+  import.meta.env?.VITE_API_BASE_URL || "https://results-package-physics-arthritis.trycloudflare.com/api";
   // import.meta.env?.VITE_API_BASE_URL || "http://localhost:5000/api";
 
 // Root backend URL without trailing /api
